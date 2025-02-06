b3a2d93c246bf08a91b75073438acdf9515e3335
ana borja <anacristina.borja@tajamar365.com>
2025-02-06 09:15:11 +0100

    subir2

--- chat-web.py
+++ chat-web.py
@@ -37,11 +37,11 @@ st.sidebar.header("Ejemplos de preguntas")
 
 # Lista de preguntas con estilos
 questions = {
-    "¿Cuáles son las curiosidades más interesantes del rugby?", "#ff5722",
-    "¿Qué ciudades son anfitrionas de la Rugby World Cup 2027?", "#e67e22",
-    "¿Cuántos equipos participan?", "#e59866",
-    "¿Las fechas de la clasificación de Europa?", "#f0b27a",
-    "¿Qué anunció World Rugby el 30 de enero de 2025 sobre las ciudades anfitrionas?", "#f5b041"
+    "¿Cuáles son las curiosidades más interesantes del rugby?",
+    "¿Qué ciudades son anfitrionas de la Rugby World Cup 2027?",
+    "¿Cuántos equipos participan?",
+    "¿Las fechas de la clasificación de Europa?",
+    "¿Qué anunció World Rugby el 30 de enero de 2025 sobre las ciudades anfitrionas?"
 }
 
 # HTML dinámico para cada pregunta con el color correspondiente y funcionalidad de clic
